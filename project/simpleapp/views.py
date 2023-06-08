4ef36c9ad8386b2fece993f843817b4fcdf277bf
Darya Kuznetsova <127608330+dasha532@users.noreply.github.com>
2023-06-08 23:30:07 +0300

    Delete project directory

--- project/simpleapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView
-from .models import Product
-
-
-class ProductsList(ListView):
-    model = Product
-    ordering = 'name'
-    template_name = 'products.html'
-    context_object_name = 'products'
