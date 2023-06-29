23334cbf37185ce5091b382ee5e7f795d1f8e78e
Darya Kuznetsova <127608330+dasha532@users.noreply.github.com>
2023-06-29 20:48:40 +0300

    Delete project directory

--- project/simpleapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProductsList, ProductDetail
-
-
-urlpatterns = [
-   path('', ProductsList.as_view()),
-   path('<int:pk>', ProductDetail.as_view()),
-]
